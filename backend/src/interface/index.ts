ffb600e61d140d5b0cc01b9fe81cbd7519774535
byamb4 <byamba4life@gmail.com>
2021-12-17 23:00:36 +0800

    [emit] vote related actions :tada:

--- backend/src/interface/index.ts
+++ backend/src/interface/index.ts
@@ -1,5 +1,5 @@
 export interface IUser {
-  username: string;
+  name: string;
   image: string;
   password: string;
   whichTeam: string;
--- backend/src/socket.ts
+++ backend/src/socket.ts
@@ -1,29 +1,65 @@
 import { Server, Socket } from 'socket.io';
-import { IUser } from 'interface';
+import { IUser, ITeam } from 'interface';
 import EVENTS from './config/events';
 
 const socket = ({ io, client }: { io: Server; client: any }) => {
   io.on(EVENTS.connection, (socket: Socket) => {
-    // socket.on(EVENTS.CLIENT.SEND_MESSAGE, (message: IMessage) => {
-    //   saveMessage(message, client);
-    //   (async () => {
-    //     const data = await getMessages(client);
-    //     socket.emit(EVENTS.SERVER.SEND_MESSAGES, data);
-    //   })();
-    // });
     socket.on(EVENTS.CLIENT.VOTE_TEAM, ({ user, team }: { user: Partial<IUser>; team: string }) => {
-      console.log(user, team);
       voteTeam(user, team, client);
-      console.log('done');
+      (async () => {
+        const data = await currentVote(client);
+        socket.emit(EVENTS.SERVER.CURRENT_VOTES, data);
+      })();
+    });
+
+    socket.on(EVENTS.CLIENT.CURRENT_VOTES, () => {
+      (async () => {
+        const data = await currentVote(client);
+        socket.emit(EVENTS.SERVER.CURRENT_VOTES, data);
+      })();
     });
   });
 };
 
 const voteTeam = async (user: Partial<IUser>, team: string, client: any) => {
-  const record = await client.get(`user_${user.username}`);
-  const userData = JSON.parse(record);
-  userData.votedTeam = team;
-  await client.set(`user_${user.username}`, JSON.stringify(userData));
+  const record = await client.get(`user_${user.name}`);
+  if (record) {
+    const userData = JSON.parse(record);
+    userData.votedTeam = team;
+    await client.set(`user_${user.name}`, JSON.stringify(userData));
+  }
+};
+
+const currentVote = async (client: any) => {
+  const userKeys = await client.keys('user_*');
+  const teamKeys = await client.keys('team_*');
+
+  const users = await Promise.all(
+    userKeys.map(async (key: string) => {
+      const record = await client.get(key);
+      const userData = JSON.parse(record);
+      return userData;
+    }),
+  );
+
+  const resp: { team: ITeam; users: IUser }[] = await Promise.all(
+    teamKeys.map(async (key: string) => {
+      const team = await client.get(key);
+      const teamData = JSON.parse(team);
+      const votedUsers: IUser[] = [];
+
+      users.forEach((user: IUser) => {
+        if (user.votedTeam.toLowerCase() === teamData.name.toLowerCase()) {
+          votedUsers.push(user);
+        }
+      });
+      return {
+        team: teamData,
+        users: votedUsers,
+      };
+    }),
+  );
+  return resp;
 };
 
 // const saveMessage = async (message: IMessage, client: any) => {
